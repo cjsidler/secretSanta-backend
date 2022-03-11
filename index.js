9981b1b33157cd1dd772b07416a0c9354993d821
Collin <cjsidler@gmail.com>
2022-03-11 13:57:41 -0800

    fixes cors options

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV !== "production") {
     const cors = require("cors");
     app.use(cors());
 } else {
+    const cors = require("cors");
     const corsOptions = {
         origin: "https://secret-santa-app-frontend.herokuapp.com/",
         optionsSuccessStatus: 200,
